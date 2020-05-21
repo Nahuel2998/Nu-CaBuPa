27cd5987ce68dbfa35547773d1dd753d32e52e6b
Manuel <jmbh076@gmail.com>
2020-05-21 17:50:48 -0300

    Comprobacion
    -Desactualizacion del concetor debido a incompatibilidad del framework 4.8 - 4.5
    +Conector en uso 6.1, framework 4.0

--- Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Form1.Designer.vb
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Form1.Designer.vb
@@ -22,25 +22,28 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim TreeNode1 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Seccion1")
-        Dim TreeNode2 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Modulo1", New System.Windows.Forms.TreeNode() {TreeNode1})
+        Dim TreeNode11 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Seccion1")
+        Dim TreeNode12 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Modulo1", New System.Windows.Forms.TreeNode() {TreeNode11})
         Me.Label1 = New System.Windows.Forms.Label()
         Me.CMenu = New System.Windows.Forms.Panel()
-        Me.Status = New System.Windows.Forms.Panel()
-        Me.Content = New System.Windows.Forms.Panel()
-        Me.Title = New System.Windows.Forms.Panel()
-        Me.TreeView1 = New System.Windows.Forms.TreeView()
         Me.TreeMenu = New System.Windows.Forms.GroupBox()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Status = New System.Windows.Forms.Panel()
         Me.Estado = New System.Windows.Forms.Label()
+        Me.Content = New System.Windows.Forms.Panel()
+        Me.Dibujo = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Title = New System.Windows.Forms.Panel()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.CMenu.SuspendLayout()
+        Me.TreeMenu.SuspendLayout()
         Me.Status.SuspendLayout()
         Me.Content.SuspendLayout()
         Me.Title.SuspendLayout()
-        Me.TreeMenu.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -48,7 +51,7 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(31, 87)
+        Me.Label1.Location = New System.Drawing.Point(4, 70)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(328, 73)
         Me.Label1.TabIndex = 0
@@ -64,58 +67,6 @@ Partial Class Form1
         Me.CMenu.Size = New System.Drawing.Size(134, 234)
         Me.CMenu.TabIndex = 1
         '
-        'Status
-        '
-        Me.Status.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Status.AutoSize = True
-        Me.Status.Controls.Add(Me.Estado)
-        Me.Status.Location = New System.Drawing.Point(13, 254)
-        Me.Status.Name = "Status"
-        Me.Status.Size = New System.Drawing.Size(527, 34)
-        Me.Status.TabIndex = 2
-        '
-        'Content
-        '
-        Me.Content.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-                    Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Content.Controls.Add(Me.Label4)
-        Me.Content.Controls.Add(Me.Label3)
-        Me.Content.Controls.Add(Me.Title)
-        Me.Content.Controls.Add(Me.Label1)
-        Me.Content.Location = New System.Drawing.Point(153, 13)
-        Me.Content.Name = "Content"
-        Me.Content.Size = New System.Drawing.Size(387, 234)
-        Me.Content.TabIndex = 3
-        '
-        'Title
-        '
-        Me.Title.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Title.Controls.Add(Me.GroupBox1)
-        Me.Title.Location = New System.Drawing.Point(4, 6)
-        Me.Title.Name = "Title"
-        Me.Title.Size = New System.Drawing.Size(380, 48)
-        Me.Title.TabIndex = 1
-        '
-        'TreeView1
-        '
-        Me.TreeView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-                    Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TreeView1.Location = New System.Drawing.Point(6, 19)
-        Me.TreeView1.Name = "TreeView1"
-        TreeNode1.Name = "Seccion1"
-        TreeNode1.Tag = "Seccion1"
-        TreeNode1.Text = "Seccion1"
-        TreeNode2.Name = "Modulo1"
-        TreeNode2.Tag = "Modulo1"
-        TreeNode2.Text = "Modulo1"
-        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode2})
-        Me.TreeView1.Size = New System.Drawing.Size(115, 202)
-        Me.TreeView1.TabIndex = 0
-        '
         'TreeMenu
         '
         Me.TreeMenu.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
@@ -129,6 +80,34 @@ Partial Class Form1
         Me.TreeMenu.TabStop = False
         Me.TreeMenu.Text = "Menu"
         '
+        'TreeView1
+        '
+        Me.TreeView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TreeView1.Location = New System.Drawing.Point(6, 19)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode11.Name = "Seccion1"
+        TreeNode11.Tag = "Seccion1"
+        TreeNode11.Text = "Seccion1"
+        TreeNode12.Name = "Modulo1"
+        TreeNode12.Tag = "Modulo1"
+        TreeNode12.Text = "Modulo1"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode12})
+        Me.TreeView1.Size = New System.Drawing.Size(115, 202)
+        Me.TreeView1.TabIndex = 0
+        '
+        'Status
+        '
+        Me.Status.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Status.AutoSize = True
+        Me.Status.Controls.Add(Me.Estado)
+        Me.Status.Location = New System.Drawing.Point(13, 254)
+        Me.Status.Name = "Status"
+        Me.Status.Size = New System.Drawing.Size(527, 34)
+        Me.Status.TabIndex = 2
+        '
         'Estado
         '
         Me.Estado.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
@@ -139,6 +118,65 @@ Partial Class Form1
         Me.Estado.Size = New System.Drawing.Size(514, 13)
         Me.Estado.TabIndex = 0
         Me.Estado.Text = "-Status"
+        '
+        'Content
+        '
+        Me.Content.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Content.Controls.Add(Me.TextBox1)
+        Me.Content.Controls.Add(Me.Button1)
+        Me.Content.Controls.Add(Me.Dibujo)
+        Me.Content.Controls.Add(Me.Label4)
+        Me.Content.Controls.Add(Me.Label3)
+        Me.Content.Controls.Add(Me.Title)
+        Me.Content.Controls.Add(Me.Label1)
+        Me.Content.Location = New System.Drawing.Point(153, 13)
+        Me.Content.Name = "Content"
+        Me.Content.Size = New System.Drawing.Size(387, 234)
+        Me.Content.TabIndex = 3
+        '
+        'Dibujo
+        '
+        Me.Dibujo.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.Dibujo.Enabled = False
+        Me.Dibujo.Font = New System.Drawing.Font("Arial Black", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Dibujo.Location = New System.Drawing.Point(104, 147)
+        Me.Dibujo.Multiline = True
+        Me.Dibujo.Name = "Dibujo"
+        Me.Dibujo.ReadOnly = True
+        Me.Dibujo.Size = New System.Drawing.Size(82, 78)
+        Me.Dibujo.TabIndex = 4
+        Me.Dibujo.Text = "NNN" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "NNN" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "NNN"
+        Me.Dibujo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 147)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "By: Manuel"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 130)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(117, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Corregir y no juzgar T.T"
+        '
+        'Title
+        '
+        Me.Title.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Title.Controls.Add(Me.GroupBox1)
+        Me.Title.Location = New System.Drawing.Point(4, 6)
+        Me.Title.Name = "Title"
+        Me.Title.Size = New System.Drawing.Size(380, 48)
+        Me.Title.TabIndex = 1
         '
         'GroupBox1
         '
@@ -165,23 +203,28 @@ Partial Class Form1
         Me.Label2.Text = "Tit"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Label3
+        'Button1
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(44, 182)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(117, 13)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Corregir y no juzgar T.T"
+        Me.Button1.Location = New System.Drawing.Point(201, 176)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Generar"
+        Me.Button1.UseVisualStyleBackColor = True
         '
-        'Label4
+        'TextBox1
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(47, 199)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(60, 13)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "By: Manuel"
+        Me.TextBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Font = New System.Drawing.Font("Arial Black", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(292, 147)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(82, 78)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.Text = "NNN" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "NNN" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "NNN"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'Form1
         '
@@ -195,11 +238,11 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "Inicio"
         Me.CMenu.ResumeLayout(False)
+        Me.TreeMenu.ResumeLayout(False)
         Me.Status.ResumeLayout(False)
         Me.Content.ResumeLayout(False)
         Me.Content.PerformLayout()
         Me.Title.ResumeLayout(False)
-        Me.TreeMenu.ResumeLayout(False)
         Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -208,7 +251,6 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents CMenu As System.Windows.Forms.Panel
     Friend WithEvents TreeMenu As System.Windows.Forms.GroupBox
-    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
     Friend WithEvents Status As System.Windows.Forms.Panel
     Friend WithEvents Content As System.Windows.Forms.Panel
     Friend WithEvents Title As System.Windows.Forms.Panel
@@ -217,5 +259,9 @@ Partial Class Form1
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents Dibujo As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 
 End Class
--- Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Form1.vb
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Form1.vb
@@ -1,6 +1,26 @@
 ﻿Public Class Form1
+    Dim R As New Random
+    Dim myArray = {"NNN", "NNN", "NNN"}
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-    Private Sub Estado_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Estado.Click
+
+
 
     End Sub
+
+    Private Sub Dibujo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Dibujo.TextChanged
+        For i = 0 To 2
+
+            myArray(i) = (R.Next(0, 2)).ToString
+            myArray(i) += (R.Next(0, 2)).ToString
+            myArray(i) += R.Next(0, 2).ToString
+        Next
+        Dibujo.Lines = myArray
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dibujo_Click(sender, e)
+        TextBox1.Lines = myArray
+    End Sub
+
 End Class
