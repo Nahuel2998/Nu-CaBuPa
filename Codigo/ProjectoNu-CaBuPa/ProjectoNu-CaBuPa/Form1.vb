b854156e7b7da56b33ca7e553532624e6aab511f
Matty-Star <62342451+Matty-Star@users.noreply.github.com>
2020-06-03 19:57:54 -0300

    Regelos de interfaz
    +Boton, agregar, borrar, editar. (falta conectar con el principal)

---
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+   
+    
+    
+    Private Sub ToolStrip1_ItemClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.ToolStripItemClickedEventArgs) Handles ToolStrip1.ItemClicked
+
+    End Sub
+
+    
+End Class
