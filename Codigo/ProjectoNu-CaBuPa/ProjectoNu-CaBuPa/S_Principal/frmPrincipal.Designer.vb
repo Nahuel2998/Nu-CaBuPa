8473a3d4ba6e40ec1498934baa9833a17d0854eb
Manuel Buslon <41681538+Shadowsx3@users.noreply.github.com>
2020-08-07 22:42:38 -0300

    Software
    with Nahuel Pacheco

--- Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/S_Principal/frmPrincipal.Designer.vb
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/S_Principal/frmPrincipal.Designer.vb
@@ -22,215 +22,156 @@ Partial Class frmPrincipal
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim TreeNode3 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Seccion1")
-        Dim TreeNode4 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Modulo1", New System.Windows.Forms.TreeNode() {TreeNode3})
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.CMenu = New System.Windows.Forms.Panel()
-        Me.TreeMenu = New System.Windows.Forms.GroupBox()
-        Me.TreeView1 = New System.Windows.Forms.TreeView()
-        Me.Status = New System.Windows.Forms.Panel()
-        Me.Estado = New System.Windows.Forms.Label()
-        Me.Content = New System.Windows.Forms.Panel()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Title = New System.Windows.Forms.Panel()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmPrincipal))
+        Me.pMain = New System.Windows.Forms.TableLayoutPanel()
+        Me.spNotEven = New System.Windows.Forms.TableLayoutPanel()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.CMenu.SuspendLayout()
-        Me.TreeMenu.SuspendLayout()
-        Me.Status.SuspendLayout()
-        Me.Content.SuspendLayout()
-        Me.Title.SuspendLayout()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.pStatus = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.pMain.SuspendLayout()
+        Me.spNotEven.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Label1
+        'pMain
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(38, 88)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(485, 108)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "En Diseño"
+        resources.ApplyResources(Me.pMain, "pMain")
+        Me.pMain.Controls.Add(Me.spNotEven, 2, 0)
+        Me.pMain.Controls.Add(Me.TableLayoutPanel1, 0, 0)
+        Me.pMain.Controls.Add(Me.TabControl1, 1, 0)
+        Me.pMain.Name = "pMain"
         '
-        'CMenu
+        'spNotEven
         '
-        Me.CMenu.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.CMenu.Controls.Add(Me.TreeMenu)
-        Me.CMenu.Location = New System.Drawing.Point(20, 20)
-        Me.CMenu.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.CMenu.Name = "CMenu"
-        Me.CMenu.Size = New System.Drawing.Size(201, 360)
-        Me.CMenu.TabIndex = 1
+        resources.ApplyResources(Me.spNotEven, "spNotEven")
+        Me.spNotEven.Controls.Add(Me.GroupBox3, 0, 1)
+        Me.spNotEven.Name = "spNotEven"
         '
-        'TreeMenu
+        'GroupBox3
         '
-        Me.TreeMenu.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TreeMenu.Controls.Add(Me.TreeView1)
-        Me.TreeMenu.Location = New System.Drawing.Point(6, 6)
-        Me.TreeMenu.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.TreeMenu.Name = "TreeMenu"
-        Me.TreeMenu.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.TreeMenu.Size = New System.Drawing.Size(190, 349)
-        Me.TreeMenu.TabIndex = 1
-        Me.TreeMenu.TabStop = False
-        Me.TreeMenu.Text = "Menu"
+        resources.ApplyResources(Me.GroupBox3, "GroupBox3")
+        Me.GroupBox3.Controls.Add(Me.TextBox1)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.TabStop = False
         '
-        'TreeView1
+        'TextBox1
         '
-        Me.TreeView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TreeView1.Location = New System.Drawing.Point(9, 29)
-        Me.TreeView1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.TreeView1.Name = "TreeView1"
-        TreeNode3.Name = "Seccion1"
-        TreeNode3.Tag = "Seccion1"
-        TreeNode3.Text = "Seccion1"
-        TreeNode4.Name = "Modulo1"
-        TreeNode4.Tag = "Modulo1"
-        TreeNode4.Text = "Modulo1"
-        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode4})
-        Me.TreeView1.Size = New System.Drawing.Size(170, 309)
-        Me.TreeView1.TabIndex = 0
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.Name = "TextBox1"
         '
-        'Status
+        'TableLayoutPanel1
         '
-        Me.Status.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Status.AutoSize = True
-        Me.Status.Controls.Add(Me.Estado)
-        Me.Status.Location = New System.Drawing.Point(20, 391)
-        Me.Status.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Status.Name = "Status"
-        Me.Status.Size = New System.Drawing.Size(790, 52)
-        Me.Status.TabIndex = 2
+        resources.ApplyResources(Me.TableLayoutPanel1, "TableLayoutPanel1")
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         '
-        'Estado
+        'TabControl1
         '
-        Me.Estado.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Estado.Location = New System.Drawing.Point(15, 17)
-        Me.Estado.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Estado.Name = "Estado"
-        Me.Estado.Size = New System.Drawing.Size(771, 20)
-        Me.Estado.TabIndex = 0
-        Me.Estado.Text = "-Status"
+        resources.ApplyResources(Me.TabControl1, "TabControl1")
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
         '
-        'Content
+        'TabPage1
         '
-        Me.Content.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Content.Controls.Add(Me.Label4)
-        Me.Content.Controls.Add(Me.Label1)
-        Me.Content.Controls.Add(Me.Label3)
-        Me.Content.Controls.Add(Me.Title)
-        Me.Content.Location = New System.Drawing.Point(230, 20)
-        Me.Content.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Content.Name = "Content"
-        Me.Content.Size = New System.Drawing.Size(580, 360)
-        Me.Content.TabIndex = 3
+        resources.ApplyResources(Me.TabPage1, "TabPage1")
+        Me.TabPage1.Controls.Add(Me.TableLayoutPanel2)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'Label4
+        'TableLayoutPanel2
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(57, 226)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(87, 20)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "By: Manuel"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(52, 200)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(171, 20)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Corregir y no juzgar T.T"
-        '
-        'Title
-        '
-        Me.Title.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Title.Controls.Add(Me.GroupBox1)
-        Me.Title.Location = New System.Drawing.Point(6, 9)
-        Me.Title.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Title.Name = "Title"
-        Me.Title.Size = New System.Drawing.Size(570, 74)
-        Me.Title.TabIndex = 1
+        resources.ApplyResources(Me.TableLayoutPanel2, "TableLayoutPanel2")
+        Me.TableLayoutPanel2.Controls.Add(Me.GroupBox1, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.GroupBox2, 1, 0)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
         '
         'GroupBox1
         '
-        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Location = New System.Drawing.Point(6, 6)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+        Me.GroupBox1.Controls.Add(Me.ListView1)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.GroupBox1.Size = New System.Drawing.Size(560, 63)
-        Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Title Modulo"
         '
-        'Label2
+        'ListView1
         '
-        Me.Label2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Label2.Location = New System.Drawing.Point(9, 25)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(540, 20)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "Titulo"
-        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        resources.ApplyResources(Me.ListView1, "ListView1")
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.List
+        '
+        'GroupBox2
+        '
+        resources.ApplyResources(Me.GroupBox2, "GroupBox2")
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.TabStop = False
+        '
+        'TabPage2
+        '
+        resources.ApplyResources(Me.TabPage2, "TabPage2")
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'pStatus
+        '
+        resources.ApplyResources(Me.pStatus, "pStatus")
+        Me.pStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pStatus.Name = "pStatus"
+        '
+        'Panel1
+        '
+        resources.ApplyResources(Me.Panel1, "Panel1")
+        Me.Panel1.Controls.Add(Me.pMain)
+        Me.Panel1.Controls.Add(Me.pStatus)
+        Me.Panel1.Name = "Panel1"
         '
         'frmPrincipal
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.AutoSize = True
-        Me.ClientSize = New System.Drawing.Size(826, 458)
-        Me.Controls.Add(Me.Content)
-        Me.Controls.Add(Me.Status)
-        Me.Controls.Add(Me.CMenu)
-        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Name = "frmPrincipal"
-        Me.Text = "Inicio"
-        Me.CMenu.ResumeLayout(False)
-        Me.TreeMenu.ResumeLayout(False)
-        Me.Status.ResumeLayout(False)
-        Me.Content.ResumeLayout(False)
-        Me.Content.PerformLayout()
-        Me.Title.ResumeLayout(False)
+        Me.pMain.ResumeLayout(False)
+        Me.spNotEven.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TableLayoutPanel2.ResumeLayout(False)
         Me.GroupBox1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents CMenu As System.Windows.Forms.Panel
-    Friend WithEvents TreeMenu As System.Windows.Forms.GroupBox
-    Friend WithEvents Status As System.Windows.Forms.Panel
-    Friend WithEvents Content As System.Windows.Forms.Panel
-    Friend WithEvents Title As System.Windows.Forms.Panel
-    Friend WithEvents Estado As System.Windows.Forms.Label
-    Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
 
+    Friend WithEvents pMain As TableLayoutPanel
+    Friend WithEvents pStatus As Panel
+    Friend WithEvents spNotEven As TableLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents GroupBox2 As GroupBox
 End Class
--- Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/S_Principal/frmPrincipal.vb
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/S_Principal/frmPrincipal.vb
@@ -1,16 +1,18 @@
-﻿Public Class frmPrincipal
-    Dim R As New Random
-    Dim myArray = {"NNN", "NNN", "NNN"}
+﻿Imports System.Drawing.Drawing2D
+
+Public Class frmPrincipal
 
     Private Sub frmPrincipal_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
         ModConector.desconectar()
     End Sub
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-
-
-
+    Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Width = Screen.PrimaryScreen.WorkingArea.Width * 0.85
+        Me.Height = Screen.PrimaryScreen.WorkingArea.Height * 0.8
     End Sub
 
-
+    Private Sub frmPrincipal_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        Dim p As Pen = New Pen(Color.Red)
+        e.Graphics.DrawRectangle(p, Panel1.Left - 1, Panel1.Top - 1, Panel1.Width + 100, Panel1.Height + 1)
+    End Sub
 End Class
