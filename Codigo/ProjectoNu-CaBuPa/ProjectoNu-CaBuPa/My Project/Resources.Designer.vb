18564a5b7be3f119c5a7052ffd06dd64200500ff
NaP <nahuel2998@gmail.com>
2020-09-15 20:14:34 -0300

    Mini-Fix al commit anterior
    -Una imagen problematica

--- Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/My Project/Resources.Designer.vb
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/My Project/Resources.Designer.vb
@@ -79,16 +79,5 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property png_clipart_black_and_yellow_exclamation_point_danger_signage_icon_danger_tape_s_angle_triangle() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("png-clipart-black-and-yellow-exclamation-point-danger-signage-icon-danger-tape-s-"& _ 
-                        "angle-triangle", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
