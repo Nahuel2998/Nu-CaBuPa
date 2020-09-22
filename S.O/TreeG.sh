36bcec2ffc0ddcd3b0774f01594da65510deccae
Manuel Buslon <41681538+Shadowsx3@users.noreply.github.com>
2020-09-22 16:07:13 -0300

    Update
    Problemas con tab

--- S.O/TreeG.sh
+++ S.O/TreeG.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
+Crear () {
+    printf "%s\n" "Desea crear un archivo o subdirectorio?" "1)Archivo" "2)Subdirectorio"
+    read OP
+    Direcciones="."
+    #Cantidad de carpetas
+    CA=0
+    RecD $TD 0
+}
+#Recorre el directorio recursivamente
+RecD () {
+    #Nivel de recurrencia
+    RL=$2
+    Espacio="\t"
+    for i in `seq 0 $RL`;do
+            Espacio=$Espacio"\t"
+        done
+    printf "$Espacio%s\n" $(ls -p $1 |egrep -v "/")
+    for i in $(ls -p $1 | egrep "/$" | sed 's/^\///'); do
+            printf "$Espacio%s)%s\n" $CA "$i"
+            Direcciones=$Direcciones":"$1"/"$i
+            CA=`expr $CA + 1`
+            Num=`expr $RL + 1`
+            RecD "$1/$i" $Num
+        done
+}
 TD=""
 OP=""
 #Se pregunta cual es la dirección en la que se creará el arbol
 printf "%s\n" "Indique la dirección en la que se creará el arbol"
 read TD
 #Verificamos que no sea camino absoluto y de que exista
-TD=$(echo $TD | sed "s/\///g") 
-if ! (( -d $TD )) ; then
+TD=$(echo $TD | sed 's/^\///') 
+TD="/home/manuel/"$TD
+if [ -d $TD ] ; then
+echo holis
+    else
     mkdir $TD
     fi
-done
-Create
-function Create {
-    printf "%s\n" "Desea crear un archivo o subdirectorio?" "1)Archivo" "2)Subdirectorio"
-    read OP
-    RecD $TD 0
-}
-#Recorre el directorio recursivamente
-function RecD(){
-    #Nivel de recurrencia
-    RL=$2
-    
-    Espacio="\t"
-    for i in `seq 0 $RL`;do
-            Espacio=$Espacio"\t"
-        done
-    for i in $(ls -p $1 | egrep "/$" | sed "s/\///"); do
-            printf "$Espacio%s\n" $i
-            printf "$Espacio%s\n" $(ls -p $1 |egrep -v "/")
-            RecD $i `expr $RL + 1`
-        done
-}
-
-
+Crear
 
