e50ebb0c2453a23626bdb286a21fedebfd9ffd24
Manuel Buslon <41681538+Shadowsx3@users.noreply.github.com>
2020-09-23 07:05:34 -0300

    Update TreeG.sh
    Bad sustitution 43

--- S.O/TreeG.sh
+++ S.O/TreeG.sh
@@ -2,37 +2,66 @@
 Crear () {
     printf "%s\n" "Desea crear un archivo o subdirectorio?" "1)Archivo" "2)Subdirectorio" "3)Salir"
     read OP
+    Eleccion=""
+    if [ $OP -eq 1 ]; then
+        Eleccion="archivo"
+    elif [ $OP -eq 2 ]; then
+        Eleccion="subdirectorio"
+    else
+    exit
+    fi
     RecD $TD
 }
-
-#Recorre el directorio recursivamente
+Nombre(){
+    printf "%s\n" "Indique un nombre para el $Eleccion"
+    read Nombre
+    if [ oP -eq 1 ]; then
+        touch "$DirTemp/$Nombre"
+    elif [ oP -eq 2 ]; then
+        mkdir "$DirTemp/$Nombre"
+    fi
+    Crear
+}
+#Recorre el directorio
 RecD () {
     printf "%s\n" "Donde desea crearlo?"
-    printf "%s\n" $(tree)
+    tree $TD
     read nombre
-    Direcciones=$(find -type d -name $nombre)
-    if [ ${#Direcciones[@]} -eq 1 ]; then
-        DirTemp=$Direcciones[0]
-    elif [ ${#Direcciones[@]} -gt 1 ]; then
+    Direcciones=$(find $TD -type d -name $nombre)
+    Result=${#Direcciones[@]}
+    if [ $Result -eq 1 ]; then
+        DirTemp="$TD/"${Direcciones[0]}
+        Nombre
+    elif [ $Result -gt 1 ]; then
         printf "%s\n" "A cual directorio se refiere?"
         Num=0
-        for dir in Direcciones; do
+        for dir in $Direcciones; do
             printf "$Num)%s\n" $dir
             Num=`expr $num + 1`
         done
-        read Opcion
-        if [ -d "Direcciones[$Num]" ]; then
-            DirTemp=$Direcciones[$Num]
+        read Num
+        eleccion=${Direcciones[$Num]}
+        echo $eleccion
+        if [ -d "$eleccion" ]; then
+            DirTemp=$eleccion
+            Nombre
         else
-            printf "%s\n" "No se encontró la opción"
+            printf "%s\n" "No se encontró la opción" "Ingrese 0 para salir"
+            read Salir
+            if [ $Salir -eq 0 ]; then
+                exit
+            else
+            	RecD
+            fi
         fi
     else
         printf "%s\n" "No se encontró el directorio" "Ingrese 0 para salir"
         read Salir
-        if [ $salir -eq 0 ]; then
+        if [ $Salir -eq 0 ]; then
             exit
+        else
+        	RecD
         fi
-        RecD
     fi
 }
 
@@ -44,10 +73,13 @@ printf "%s\n" "Indique la dirección en la que se creará el arbol"
 read TD
 #Verificamos que no sea camino absoluto y de que exista
 TD=$(echo $TD | sed 's/^\///') 
-TD="~/"$TD
-if [ -d $TD ] ; then
-    else
+TD="/home/manuel/"$TD
+if [ -d "$TD" ] ; then
+    printf "%s\n" "Directorio encontrado"
+else
+    printf "%s\n" "Directorio creado"
     mkdir $TD
-    fi
+fi
 Crear
+
 
