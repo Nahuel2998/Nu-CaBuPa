219b29078f215159eef03e3b95dc222bd57bafd8
Manuel Buslon <41681538+Shadowsx3@users.noreply.github.com>
2020-09-22 22:08:16 -0300

    Bash e Ingles
    Trabajos
    
    Co-Authored-By: MateoC4BR4L <56230105+MateoC4BR4L@users.noreply.github.com>
    Co-Authored-By: Milagros Nuñez <62342451+Matty-Star@users.noreply.github.com>
    Co-Authored-By: NaP <56210014+Nahuel2998@users.noreply.github.com>

--- S.O/TreeG.sh
+++ S.O/TreeG.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 Crear () {
-    printf "%s\n" "Desea crear un archivo o subdirectorio?" "1)Archivo" "2)Subdirectorio"
+    printf "%s\n" "Desea crear un archivo o subdirectorio?" "1)Archivo" "2)Subdirectorio" "3)Salir"
     read OP
-    Direcciones="."
-    #Cantidad de carpetas
-    CA=0
-    RecD $TD 0
+    RecD $TD
 }
+
 #Recorre el directorio recursivamente
 RecD () {
-    #Nivel de recurrencia
-    RL=$2
-    Espacio="\t"
-    for i in `seq 0 $RL`;do
-            Espacio=$Espacio"\t"
+    printf "%s\n" "Donde desea crearlo?"
+    printf "%s\n" $(tree)
+    read nombre
+    Direcciones=$(find -type d -name $nombre)
+    if [ ${#Direcciones[@]} -eq 1 ]; then
+        DirTemp=$Direcciones[0]
+    elif [ ${#Direcciones[@]} -gt 1 ]; then
+        printf "%s\n" "A cual directorio se refiere?"
+        Num=0
+        for dir in Direcciones; do
+            printf "$Num)%s\n" $dir
+            Num=`expr $num + 1`
         done
-    printf "$Espacio%s\n" $(ls -p $1 |egrep -v "/")
-    for i in $(ls -p $1 | egrep "/$" | sed 's/^\///'); do
-            printf "$Espacio%s)%s\n" $CA "$i"
-            Direcciones=$Direcciones":"$1"/"$i
-            CA=`expr $CA + 1`
-            Num=`expr $RL + 1`
-            RecD "$1/$i" $Num
-            echo $RL
-        done
+        read Opcion
+        if [ -d "Direcciones[$Num]" ]; then
+            DirTemp=$Direcciones[$Num]
+        else
+            printf "%s\n" "No se encontró la opción"
+        fi
+    else
+        printf "%s\n" "No se encontró el directorio" "Ingrese 0 para salir"
+        read Salir
+        if [ $salir -eq 0 ]; then
+            exit
+        fi
+        RecD
+    fi
 }
+
 TD=""
+DirTemp=""
 OP=""
 #Se pregunta cual es la dirección en la que se creará el arbol
 printf "%s\n" "Indique la dirección en la que se creará el arbol"
 read TD
 #Verificamos que no sea camino absoluto y de que exista
 TD=$(echo $TD | sed 's/^\///') 
-TD="/home/manuel/"$TD
+TD="~/"$TD
 if [ -d $TD ] ; then
-echo holis
     else
     mkdir $TD
     fi
