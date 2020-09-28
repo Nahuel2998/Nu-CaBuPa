775c338131bcc4b32df0657316eaebb460ab316b
Matty-Star <62342451+Matty-Star@users.noreply.github.com>
2020-09-28 20:46:27 -0300

    Interfaz Función

---
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Funcion/FmFuncion.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FmFuncion
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.BtnbuscarFunci = New System.Windows.Forms.Button()
+        Me.btneditarfunci = New System.Windows.Forms.Button()
+        Me.btningresarfunc = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.GroupBox1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 1)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(1, 2)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.35135!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.64865!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(260, 185)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(254, 125)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Mongolian Baiti", 10.2!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(8, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(84, 37)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(162, 56)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel2.ColumnCount = 3
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.37795!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.BtnbuscarFunci, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.btneditarfunci, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.btningresarfunc, 2, 0)
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 134)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(254, 48)
+        Me.TableLayoutPanel2.TabIndex = 1
+        '
+        'BtnbuscarFunci
+        '
+        Me.BtnbuscarFunci.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnbuscarFunci.Font = New System.Drawing.Font("Mongolian Baiti", 10.2!)
+        Me.BtnbuscarFunci.ForeColor = System.Drawing.Color.White
+        Me.BtnbuscarFunci.Location = New System.Drawing.Point(3, 3)
+        Me.BtnbuscarFunci.Name = "BtnbuscarFunci"
+        Me.BtnbuscarFunci.Size = New System.Drawing.Size(77, 42)
+        Me.BtnbuscarFunci.TabIndex = 0
+        Me.BtnbuscarFunci.Text = "Borrar"
+        Me.BtnbuscarFunci.UseVisualStyleBackColor = True
+        '
+        'btneditarfunci
+        '
+        Me.btneditarfunci.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btneditarfunci.Font = New System.Drawing.Font("Mongolian Baiti", 10.2!)
+        Me.btneditarfunci.ForeColor = System.Drawing.Color.White
+        Me.btneditarfunci.Location = New System.Drawing.Point(86, 3)
+        Me.btneditarfunci.Name = "btneditarfunci"
+        Me.btneditarfunci.Size = New System.Drawing.Size(81, 42)
+        Me.btneditarfunci.TabIndex = 1
+        Me.btneditarfunci.Text = "Editar Nombre"
+        Me.btneditarfunci.UseVisualStyleBackColor = True
+        '
+        'btningresarfunc
+        '
+        Me.btningresarfunc.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btningresarfunc.Font = New System.Drawing.Font("Mongolian Baiti", 10.2!)
+        Me.btningresarfunc.ForeColor = System.Drawing.Color.White
+        Me.btningresarfunc.Location = New System.Drawing.Point(173, 3)
+        Me.btningresarfunc.Name = "btningresarfunc"
+        Me.btningresarfunc.Size = New System.Drawing.Size(78, 42)
+        Me.btningresarfunc.TabIndex = 2
+        Me.btningresarfunc.Text = "Ingresar"
+        Me.btningresarfunc.UseVisualStyleBackColor = True
+        '
+        'FmFuncion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(39, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(262, 190)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "FmFuncion"
+        Me.Text = "Función"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents BtnbuscarFunci As Button
+    Friend WithEvents btneditarfunci As Button
+    Friend WithEvents btningresarfunc As Button
+End Class
---
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Funcion/FmFuncion.vb
@@ -0,0 +1,5 @@
+﻿Public Class FmFuncion
+    Private Sub FmFuncion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
