7b4ce5fab311f14190a66193e0ed3d632ee25517
Manuel Buslon <41681538+Shadowsx3@users.noreply.github.com>
2020-10-20 19:22:23 -0300

    Mostrar datos de publicidades
    Correcciones del menú

---
+++ Base de Datos/Creacion/Datos/Datos.sql
@@ -0,0 +1,35 @@
+INSERT INTO `acceso` VALUES (1,'Inicio','ac'),(2,'Eventos','ac'),(4,'ninguna','xr'),(5,'nnnn','xr'),(6,'Hola','S'),(7,'Hola','S'),(8,'Hola','S'),(9,'Hola','S'),(10,'Hola','S'),(11,'Hola','S'),(3,'Serie','ac');
+
+INSERT INTO `empresa` VALUES (1,'Mil','113','dd'),(3,'NuCaBuPA',NULL,NULL);
+
+INSERT INTO `evento` VALUES (1,'My ','None'),(2,'We','None');
+
+INSERT INTO `programa` VALUES (1,'lol','The best',NULL),(2,'A es una vocal','The best program',NULL),(3,'Meles program','The program',NULL),(4,'Relleno con Mateo','The',NULL),(5,'Proof or report Manuel','The',NULL);
+
+INSERT INTO `fechaevento` VALUES (1,'2020-12-12 00:00:00'),(1,'2020-11-12 00:00:00'),(2,'2020-11-06 00:00:00');
+
+INSERT INTO `fechaprograma` VALUES ('2020-08-10','07:14:10','18:14:10',1),('2020-08-10','06:14:10','17:14:10',1),('2020-08-10','00:14:10','12:14:10',1),('2020-08-11','09:05:00','21:20:00',3),('2020-08-11','12:30:00','00:00:13',4),('2020-08-12','13:30:00','00:00:14',5),('0000-00-00','10:30:00','14:50:00',2),('2020-10-11','10:20:00','23:10:00',2);
+
+INSERT INTO `funcion` VALUES (1,'Trabaja','Funcionar'),(2,'Trabaja','Peren'),(3,'Trabaja','Pere'),(4,'Trabaja','');
+
+INSERT INTO `funcionario` VALUES (1,'34244','Manuel',NULL),(2,'34244','Mili',NULL),(3,'9483948','Mateo',NULL),(4,'2998','Nahuel',NULL);
+
+INSERT INTO `funtrabaja` VALUES (2,2,'2020-10-07',NULL),(1,2,'2020-10-07',NULL),(3,2,'2020-10-07',NULL),(4,2,'2020-10-07',NULL),(5,2,'2020-10-07',NULL);
+
+INSERT INTO `pmuestrapubli` VALUES (1,3,'2020-04-09','2020-10-10'),(2,3,'2020-04-09','2020-10-10'),(1,4,'2020-04-09','2020-08-09'),(2,1,'2020-04-09','2020-08-10'),(3,1,'2020-02-19','2020-12-12');
+
+INSERT INTO `publicidad` VALUES (1,'De todo un poco',1,''),(2,'Nadita',2,''),(3,'Mili',1,'');
+
+INSERT INTO `serie` VALUES (10,NULL,'Voces'),(5,NULL,'holis'),(11,'2020-10-09','Seri1');
+
+INSERT INTO `tanda` VALUES ('01:00:00','03:00:00'),('03:00:00','05:00:00'),('05:00:00','10:00:00'),('10:00:00','05:00:00'),('15:00:00','16:00:00'),('16:00:00','20:00:00'),('20:00:00','24:00:00');
+INSERT INTO `aparecepubli` VALUES (1,'21:05:00','2020-08-09','2020-08-10'),(2,'21:05:00','2020-08-09','2020-08-31'),(1,'22:05:00','2020-08-09','2020-08-10'),(2,'22:05:00','2020-08-09','2020-08-31'),(1,'15:05:00','2020-08-01','2020-08-30'),(1,'05:00:00','2020-02-09','2020-08-19'),(2,'15:00:00','2020-02-09','2020-08-19'),(3,'15:00:00','2020-02-09','2020-08-19'),(2,'16:00:00','2020-02-09','2020-08-19'),(1,'20:00:00','2020-02-09','2020-08-19');
+
+INSERT INTO `trabajacomo` VALUES (1,0,0),(2,4,1),(3,3,2),(4,2,1),(5,1,3);
+
+INSERT INTO `ustieneacceso` VALUES (1,5),(1,6),(1,8),(1,9),(1,10),(2,1),(2,5),(2,6),(2,10),(2,11);
+
+INSERT INTO `usuarios` VALUES (1,'d',_binary 'å∞ê6µ\œ\Ì¶#˜B˝O†9'),(2,'m',_binary 'A†Gu\Êk\œ÷ñq0â§¸H');
+
+INSERT INTO `video` VALUES (4,'adios','hola',NULL,NULL),(3,'nofffffffdddddgddcdd','LOLd',11,'2020-08-08');
+
--- Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Modulos/ModLog.vb
+++ Codigo/ProjectoNu-CaBuPa/ProjectoNu-CaBuPa/Modulos/ModLog.vb
@@ -5,9 +5,14 @@ Module ModLog
     Private ruta As String = "..\User\"
     ':::Nombre del archivo
     Private archivo As String = "Log.txt"
+    Private ENUSU As Boolean = False
     Public Sub Guardar(ByVal Acciones As String)
-        Dim escribir As New StreamWriter(ruta & archivo, True)
-        escribir.WriteLine(Now.ToString + " " + My.Computer.Name + " ' " + Acciones + " ")
-        escribir.Close()
+        If Not ENUSU Then
+            ENUSU = True
+            Dim escribir As New StreamWriter(ruta & archivo, True)
+            escribir.WriteLine(Now.ToString + " " + My.Computer.Name + " ' " + Acciones + " ")
+            escribir.Close()
+            ENUSU = False
+        End If
     End Sub
 End Module
